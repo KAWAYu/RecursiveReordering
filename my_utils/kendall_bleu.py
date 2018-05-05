09b3fbc32aa38e1d15db429ead8ae1d93dbd5593
KAWAYu <kawara.yuki@ist.osaka-u.ac.jp>
2018-05-05 22:59:03 +0900

    kendall bleu added

--- my_utils/kendall_bleu.py
+++ my_utils/kendall_bleu.py
@@ -3,7 +3,7 @@
 
 import argparse
 import codecs
-import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 
@@ -13,7 +13,24 @@ def parse():
     parser.add_argument('tau2', help='tau2 file')
     parser.add_argument('bleu1', help='bleu1 file')
     parser.add_argument('bleu2', help='bleu2 file')
+    parser.add_argument('--fig_name', default="tau_bleu.pdf", type=str, help="figure name")
     return parser.parse_args()
+
+
+def calc_corr_coef(xs, ys):
+    """
+    相関係数、分散を計算する関数
+    :param xs: １つ目のデータ
+    :param ys: ２つ目のデータ
+    :return: 相関係数、１つ目の分散、２つ目の分散
+    """
+    avg_x = sum(xs) / len(xs)
+    avg_y = sum(ys) / len(ys)
+    numerator = sum((x - avg_x) * (y - avg_y) for x, y in zip(xs, ys))
+    x_square = sum((x - avg_x) ** 2 for x in xs)
+    y_square = sum((y - avg_y) ** 2 for y in ys)
+    denominator = math.sqrt(x_square * y_square)
+    return numerator / denominator, x_square / len(xs), y_square / len(ys)
 
 
 if __name__ == '__main__':
@@ -29,4 +46,5 @@ if __name__ == '__main__':
     plt.xlabel("diff kendall's tau")
     plt.ylabel("diff bleu")
     plt.legend()
-    plt.show()
+    plt.savefig(args.fig_name)
+    print("corr_coef: %f, variance of BLEU: %f, variance of tau: %f" % calc_corr_coef(diff_bleus, diff_taus))
