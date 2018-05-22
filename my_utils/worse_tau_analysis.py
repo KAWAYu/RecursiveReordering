2b724a5644084ae29472c097336393035d8a9150
KAWAYu <kawara.yuki@ist.osaka-u.ac.jp>
2018-05-22 14:30:58 +0900

    add miss-preorder output

---
+++ my_utils/worse_tau_analysis.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding; utf-8 -*-
+
+import argparse
+import gzip
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tree_file', help='tree file')
+    parser.add_argument('base_tau', help='base tau file')
+    parser.add_argument('btg_tau', help='btg tau file')
+    parser.add_argument('btged_file', help='reordered sentence file')
+    parser.add_argument('alignment', help='alignment file')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse()
+    with open(args.tree_file) as tree_f, open(args.base_tau) as base_f, open(args.btg_tau) as btg_f, \
+            open(args.btged_file) as btged_f, gzip.open(args.alignment, 'r') as align:
+        for tree_line, base_tau, btg_tau, btged_line, _ in zip(tree_f, base_f, btg_f, btged_f, align):
+            align_s = align.readline().strip()
+            align_t = align.readline().strip()
+            if float(base_tau) >= float(btg_tau):
+                print(tree_line)
+                print(btged_line)
+                print(align_s)
+                print(align_t)
+                print()
